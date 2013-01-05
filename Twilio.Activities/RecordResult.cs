5a6a332d62a0ba38a0df6bc70263beffecc0f711
Jerome Haltom <wasabi@larvalstage.net>
2013-01-05 14:17:14 -0600

    * No longer need DataContract.

--- Twilio.Activities/RecordResult.cs
+++ Twilio.Activities/RecordResult.cs
@@ -7,26 +7,22 @@ namespace Twilio.Activities
     /// <summary>
     /// Result of a Record operation.
     /// </summary>
-    [DataContract]
     class RecordResult : Result
     {
 
         /// <summary>
         /// The URL of the recorded audio.
         /// </summary>
-        [DataMember]
         public Uri RecordingUrl { get; set; }
 
         /// <summary>
         /// The duration of the recorded audio.
         /// </summary>
-        [DataMember]
         public TimeSpan Duration { get; set; }
 
         /// <summary>
         /// The key (if any) pressed to end the recording or 'hangup' if the caller hung up.
         /// </summary>
-        [DataMember]
         public string Digits { get; set; }
 
     }
