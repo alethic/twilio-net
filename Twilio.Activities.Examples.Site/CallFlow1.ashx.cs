6ea6204f2106eb3b88610f3f66563cbd473e9ae8
Jerome Haltom <wasabi@larvalstage.net>
2013-02-16 16:29:31 -0600

    Repaired sample.

--- Twilio.Activities.Examples.Site/CallFlow1.ashx.cs
+++ Twilio.Activities.Examples.Site/CallFlow1.ashx.cs
@@ -1,14 +1,21 @@
 ﻿using System.Activities;
+using System.Runtime.DurableInstancing;
+using System.Web.SessionState;
 
 namespace Twilio.Activities.Examples.Site
 {
 
-    public class CallFlow1 : TwilioHttpHandler
+    public class CallFlow1 : HttpHandler, IRequiresSessionState
     {
 
-        protected override Activity InitializeActivity()
+        protected override Activity CreateActivity()
         {
             return new Twilio.Activities.Examples.CallFlow1();
+        }
+
+        protected override InstanceStore CreateInstanceStore()
+        {
+            return new HttpSessionInstanceStore(Context);
         }
 
     }
